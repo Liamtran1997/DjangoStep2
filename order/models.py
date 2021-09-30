6939d819bcda1f8f4316160be0f3de34a2fb81c3
Liamtran1997 <82038695+Liamtran1997@users.noreply.github.com>
2021-09-30 20:49:52 +0700

    deleted309

--- order/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from cart.models import Cart
-# Create your models here.
-
-class Order(models.Model):
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
-    address = models.CharField(max_length=100, null=False)
-    description = models.TextField(default='')
-    is_deleted = models.BooleanField(default=False)
